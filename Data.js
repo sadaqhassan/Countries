8a2bf6e97a5f992050d8731d0817ba5d6bc58e8e
sadak hassan <sadaqsamor99@gmail.com>
2025-09-28 10:05:25 +0300

    Added data

---
+++ Data.js
@@ -0,0 +1,166 @@
+const countries = [
+  {
+    name: "Afghanistan",
+    flag: "../src/assets/Afganistan.png",
+    currency: "Afghan afghani",
+    continent: "Asia",
+    description:
+      "A landlocked country in South-Central Asia, known for its mountains and history.",
+  },
+  {
+    name: "Albania",
+    flag: "../src/assets/Albania.png",
+    currency: "Albanian lek",
+    continent: "Europe",
+    description:
+      "A small mountainous country in Southeast Europe, known for its history and landscapes.",
+  },
+  {
+    name: "Argentina",
+    flag: "../src/assets/Argentine.png",
+    currency: "Argentine peso",
+    continent: "South America",
+    description:
+      "The second-largest country in South America, famous for tango, beef, and Patagonia.",
+  },
+  {
+    name: "Brazil",
+    flag: "../src/assets/Brazil.png",
+    currency: "Brazilian real",
+    continent: "South America",
+    description:
+      "The largest country in South America, home to the Amazon rainforest and Rio de Janeiro.",
+  },
+  {
+    name: "China",
+    flag: "../src/assets/China.png",
+    currency: "Renminbi (Yuan)",
+    continent: "Asia",
+    description:
+      "The most populous country in the world, known for its long history and the Great Wall.",
+  },
+  {
+    name: "Benin",
+    flag: "../src/assets/Benin.png",
+    currency: "somaliaShiling",
+    continent: "Africa",
+    description:
+      "A small country in the Horn of Africa, strategically located near the Red Sea.",
+  },
+  {
+    name: "Ethiopia",
+    flag: "../src/assets/Ethiopia.png",
+    currency: "Ethiopian birr",
+    continent: "Africa",
+    description:
+      "One of the oldest nations in the world, known for its coffee and diverse cultures.",
+  },
+  {
+    name: "France",
+    flag: "../src/assets/France.png",
+    currency: "Euro",
+    continent: "Europe",
+    description:
+      "A Western European country, famous for art, cuisine, fashion, and the Eiffel Tower.",
+  },
+  {
+    name: "India",
+    flag: "../src/assets/India.png",
+    currency: "Indian rupee",
+    continent: "Asia",
+    description:
+      "A diverse South Asian country, known for its culture, Bollywood, and the Taj Mahal.",
+  },
+  {
+    name: "Japan",
+    flag: "../src/assets/Japan.png",
+    currency: "japanese yen",
+    continent: "Asia",
+    description:
+      "An island nation in East Asia, famous for technology, sushi, and Mount Fuji.",
+  },
+  {
+    name: "Kenya",
+    flag: "../src/assets/Kenya.png",
+    currency: "Kenyan shilling",
+    continent: "Africa",
+    description:
+      "An East African country, known for safaris, wildlife, and the Great Rift Valley.",
+  },
+  {
+    name: "Nepal",
+    flag: "../src/assets/Nepal.png",
+    currency: "Nepalese rupee",
+    continent: "Asia",
+    description:
+      "A Himalayan country, home to Mount Everest and rich cultural heritage.",
+  },
+  {
+    name: "Palestine",
+    flag: "../src/assets/Palestine.png",
+    currency:
+      "Palestinian pound (historical) / Israeli shekel & Jordanian dinar (used)",
+    continent: "Asia",
+    description:
+      "A region in Western Asia with deep historical and cultural significance.",
+  },
+  {
+    name: "Somalia",
+    flag: "../src/assets/Somalia.png",
+    currency: "Somali shilling",
+    continent: "Africa",
+    description:
+      "A Horn of Africa country with a long coastline and rich cultural traditions.",
+  },{
+    
+    name: "Somaliland",
+    flag: "../src/assets/Somaliland.png",
+    currency: "Somali shilling",
+    continent: "Africa",
+    description:
+      "A Horn of Africa country with a long coastline and rich cultural traditions.",
+  
+  },
+  {
+    name: "Turkey",
+    flag: "../src/assets/Turkey.png",
+    currency: "Turkish lira",
+    continent: "Europe & Asia",
+    description:
+      "A transcontinental country, famous for Istanbul, Cappadocia, and rich Ottoman history.",
+  },
+  {
+    name: "Yemen",
+    flag: "../src/assets/Yenmen.png",
+    currency: "Dinar",
+    continent: "Asia",
+    description:
+      "A transcontinental country, famous for Istanbul, Cappadocia, and rich Ottoman history.",
+  },
+  {
+    name: "United Kingdom",
+    flag: "../src/assets/united-kingdom.png",
+    currency: "British pound sterling",
+    continent: "Europe",
+    description:
+      "An island nation made up of England, Scotland, Wales, and Northern Ireland.",
+  },
+  {
+    name: "United States",
+    flag: "../src/assets/united-states.png",
+    currency: "US dollar",
+    continent: "North America",
+    description:
+      "A diverse nation, the third-largest in the world, known for innovation and culture.",
+  },
+  {
+    name: "Vietnam",
+    flag: "../src/assets/vietnam.png",
+    currency: "Vietnamese dong",
+    continent: "Asia",
+    description:
+      "A Southeast Asian country, famous for its history, cuisine, and Ha Long Bay.",
+  },
+];
+
+export default countries;
