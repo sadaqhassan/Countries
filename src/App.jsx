a11ff20df466adca46b82e68fd25ca338e2ffb1a
sadak hassan <sadaqsamor99@gmail.com>
2025-09-28 11:32:28 +0300

    added search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/navbar'
 import Main from './components/main'
 
 const App = () => {
+  const [inputData , setInputData] = useState("")
+
   return (
     <div className=' '>
-      <Navbar/>
+      <Navbar inputData={inputData} setInputData={setInputData}/>
       <div className='mx-10'>
-        <Main/>
+        <Main inputData={inputData} setInputData={setInputData}/>
       </div>
     </div>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import countries from '../../Data'
 
-const Main = () => {
+const Main = ({inputData}) => {
     const [Data , setData] = useState([])
     const fetchData = ()=>{
         setData(countries)
     } 
     useEffect(()=>{
         fetchData()
-    },[Data])
-  return (
+    },[])
+
+    console.log(inputData)
+    
+
+    const [filered , setFiltered] = useState([])
+
+
+    useEffect(()=>{
+        if(inputData){
+            setFiltered(Data.filter(fil =>fil.name.toLowerCase().includes(inputData.toLowerCase())))
+        }else{
+        setFiltered([])
+    }
+    },[inputData,Data])
+
+return (
     <div className='mt-10 '>
         <div className='grid grid-cols-2 md:grid-cols-4 rounded gap-4 text-gray-900'>
-            { 
-            Data.map((country,index)=>(
+        {
+            filered.length > 0
+            ? filered.map((co,index)=>(
+            <div key={index} className='py-2 px-2 rounded'>
+            <p>{index+1}</p>    
+            <p className='text-center py-2 text-xl font-medium'>{co.name}</p>
+            <img className='h-56 w-56 rounded-2xl hover:scale-108 shadow-2xl' src={co.flag} alt="" />
+            <div className='flex justify-between items-center '>
+            <p className='text-sm p-2'>Continent:{co.continent}</p>
+            <p className='text-sm p-2 bg-green-600 px-2 text-white font-sm rounded'>Currency:{co.currency.slice(0,6)} 💵</p>
+            </div>
+            </div>
+            ))
+            
+            
+        
+            :Data.map((country,index)=>(
             <div key={index} className='py-2 px-2 rounded'>
             <p>{index+1}</p>    
             <p className='text-center py-2 text-xl font-medium'>{country.name}</p>
@@ -21,14 +51,14 @@ const Main = () => {
             <div className='flex justify-between items-center '>
             <p className='text-sm p-2'>Continent:{country.continent}</p>
             <p className='text-sm p-2 bg-green-600 px-2 text-white font-sm rounded'>Currency:{country.currency.slice(0,6)} 💵</p>
-
             </div>
             <p className='text-sm'>{country.description}</p>
             </div>
-            ))}
+            ))
+        }
         </div>
     </div>
-  )
+)
 }
 
 export default Main
